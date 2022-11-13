2ba150d6538dc290ab9723b1469b7cf0530acd9c
DionelExe <dionel412@gmail.com>
2022-11-13 20:14:16 -0300

    pickle

---
+++ Python/pickle.py
@@ -0,0 +1,22 @@
+from pickle import dump, load
+
+class Vehiculo:
+    marca=''
+    placa=''
+
+    def __init__(self,marca,placa):
+        self.marca=marca
+        self.placa=placa
+    def getMarca(self):
+        return self.marca
+v=Vehiculo('Aveo','AJ123')
+
+datos=open('d.bin','wb')
+pickle.dump(v,datos)
+datos.close()
+
+datos=open('d.bin','rb')
+p=pickle.load(datos)
+datos.close()
+
+print(p.getMarca(),p.placa)
